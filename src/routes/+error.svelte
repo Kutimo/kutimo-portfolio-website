abb35c3a1b83f6b2ba7a84747eae14ec2fb98060
Kutimo <marius-oliver@hotmail.com>
2022-10-02 03:07:53 +0200

    kutimo.dev

---
+++ src/routes/+error.svelte
@@ -0,0 +1,119 @@
+<script>
+	import Globalcss from '../lib/componets/globalcss.svelte';
+</script>
+
+<div class="error">
+	<img src="/images/bracket_left.svg" alt="bracket" />
+	<h1>
+		40<span class="flick">4</span> <br />
+		<span>page not found </span>
+	</h1>
+	<img class="swing" src="/images/bracket_right.svg" alt="bracket" />
+</div>
+
+<style>
+	.error {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+	}
+	h1 {
+		text-align: center;
+	}
+	span {
+		color: var(--light_color);
+	}
+	img {
+		height: 30%;
+		margin-top: 40px;
+		padding: 20px;
+	}
+	.flick {
+        -webkit-animation: flicker-1 0.9s linear infinite backwards;
+	        animation: flicker-1 1.9s linear infinite backwards;
+	}
+   
+	.swing {
+		margin-top: 24%;
+		animation: swing ease-in-out 2s infinite alternate;
+		transform-origin: left -20px;
+	}
+
+	.swing:after {
+		transform: rotate(45deg);
+	}
+
+	@keyframes swing {
+		0% {
+			transform: rotate(6deg);
+		}
+		100% {
+			transform: rotate(-6deg);
+		}
+	}
+
+
+
+    @-webkit-keyframes flicker-1 {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  41.99% {
+    opacity: 1;
+  }
+  42% {
+    opacity: 0;
+  }
+  43% {
+    opacity: 0;
+  }
+  43.01% {
+    opacity: 1;
+  }
+  47.99% {
+    opacity: 1;
+  }
+  48% {
+    opacity: 0;
+  }
+  49% {
+    opacity: 0;
+  }
+  49.01% {
+    opacity: 1;
+  }
+}
+@keyframes flicker-1 {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  41.99% {
+    opacity: 1;
+  }
+  42% {
+    opacity: 0;
+  }
+  43% {
+    opacity: 0;
+  }
+  43.01% {
+    opacity: 1;
+  }
+  47.99% {
+    opacity: 1;
+  }
+  48% {
+    opacity: 0;
+  }
+  49% {
+    opacity: 0;
+  }
+  49.01% {
+    opacity: 1;
+  }
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,29 @@
 <script>
-	import Globalcss from './globalcss.svelte';
+	import Globalcss from '../lib/componets/globalcss.svelte';
 	import Header from '../lib/componets/Header.svelte';
 	import Side_nav_left from '../lib/componets/Side_nav_left.svelte';
 	import Side_nav_right from '../lib/componets/Side_nav_right.svelte';
+	import Main from '../lib/componets/Main.svelte';
 	
 </script>
 
 <Header />
 	<Side_nav_left />
 	<Side_nav_right/>
-	<main>
-	<p>Hello, I am</p>
-	<h1>
-		Marius Kristiansen. <br />
-		I design <span class="webthings">web things.</span>
-	</h1>
-	<p class="intro_text">
-		Aspiring web developer that has a nose for details and the small things that make a big impact for your project and brand. 
-	</p>
-	<button type="button">Check out my <br> previous builds and <br> projects!</button>
-</main>
+	<Main />
 
-<style>
-	main {
-	display: flex;
-	align-items: flex-start;
-	flex-direction: column;
-	width: 70%;
-	margin: 5% auto;
-	
-}
-	p,
-	h1 {
-		color: var(--light_color);
-		
-	}
-	.webthings {
-		color: var(--secondary_color);
-	}
-    p{
-        
-        color: var(--light_color);
-		width: 30ch;
-    }
-
-	.intro_text{
-		padding: 15px;
-	}
-
-button {
-	background-color: transparent;
-	color: var(--light_color);
-	border: 1px solid var(--light_color);
-	padding: 10px;
-	height: 30%;
-	font-size: 1.5rem;
-	margin-top: 1%;
-	width: 30%;
-}
-button:hover {
-	box-shadow: 8px 8px 0px 10px rgba(0, 0, 0, 0.25);
-    transition: all 0.3s ease-out;
-	cursor: pointer; 
-}
-</style>
+	<section>
+<h2>About me</h2>
+<p>
+I’m an aspiring Front-End Developer located in Norway.I have a serious passion for design, and for creating intuitive, dynamic and of course responsive.
+</p>
+<p>
+After almost 10 years in the hospitality industry (specialising in wine, bar and fine dining ). I decided to take my talents into a different industry where I strongly believe my problem solving skills, an eye for detail and interpersonal skills that are very much transferrable.
+</p>
+<p>
+Interested in the entire frontend spectrum and working on ambitious projects with like minded people.
+</p>
+<p>
+Let’s make something exceptional together!
+</p>
+	</section>
